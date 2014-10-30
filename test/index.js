396207a6652e0b6bdc1f0adcdd76edb4c408fb77
Wyatt Preul <wpreul@gmail.com>
2014-10-30 15:35:20 -0500

    Initial structure

---
+++ test/index.js
@@ -0,0 +1,18 @@
+// Load modules
+
+var Code = require('code');
+var Lab = require('lab');
+var Jenny = require('../');
+
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+
+describe('Jenny', function () {
+
+});

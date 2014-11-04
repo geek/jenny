9b9782814e37c2f0ab35d9e1308902782de28e02
Wyatt Preul <wpreul@gmail.com>
2014-11-04 16:55:20 -0600

    Adding commander test

---
+++ test/commander.js
@@ -0,0 +1,39 @@
+// Load modules
+
+var Code = require('code');
+var Hapi = require('hapi');
+var Lab = require('lab');
+var Jenny = require('../');
+
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+
+describe('Commander', function () {
+
+    it('sends reported data to remote url', function (done) {
+
+        var server = new Hapi.Server(0);
+        server.route({ method: 'post', path: '/', handler: function (request, reply) {
+
+            expect(request.payload.test).to.equal('data');
+            reply('ok');
+        }});
+
+        server.start(function (err) {
+
+            expect(err).to.not.exist();
+            var commander = new Jenny.Commander('http://localhost:' + server.info.port);
+            commander.report({ test: 'data'}, function (err) {
+
+                expect(err).to.not.exist();
+                done();
+            });
+        });
+    });
+});
